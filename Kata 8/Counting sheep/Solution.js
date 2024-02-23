84c6f37b88f68ba88b2b4075b3ea72f983485cfd
Slameich-web <slameacer@mail.ru>
2024-02-23 10:56:22 +0300

    fix Counting sheep

--- Kata 8/Counting sheep/Solution.js
+++ Kata 8/Counting sheep/Solution.js
@@ -1,8 +1,6 @@
 const countSheeps = (sheep) => {
   let initialValue = 0;
   return sheep.reduce((currentSum, currentNumber) => {
-    console.log(currentSum);
-    console.log(currentNumber);
     if (currentNumber) {
       return (currentSum += 1);
     } else {
