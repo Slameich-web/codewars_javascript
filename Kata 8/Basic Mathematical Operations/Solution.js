01262791a312dc2bdfff9cbc28a381e50a0541d7
Slameich-web <slameacer@mail.ru>
2024-03-05 14:39:28 +0300

    Basic Mathematical Operations

---
+++ Kata 8/Basic Mathematical Operations/Solution.js
@@ -0,0 +1,14 @@
+const basicOp = (operation, value1, value2) => {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+    default:
+      return "Please input correct operator";
+  }
+};
