691b31cbb46e3f47b400838bbe499f61c72f9951
Slameich-web <slameacer@mail.ru>
2024-04-10 23:50:35 +0300

    fix

--- Kata 6/Your order, please/Solution.js
+++ Kata 6/Your order, please/Solution.js
@@ -1,6 +1,7 @@
 const order = (words) => {
   const array = words.split(" ");
   let sortedArray = [];
+
   for (let i = 0; i <= array.length; i++) {
     for (let j = 0; j < array.length; j++) {
       if (array[j].indexOf(i) >= 0) {
