6c9cefcae7f023fbf1fdacd5de79e4197c9e5e3c
Slameich-web <SlameACER@mail.ru>
2024-06-10 12:55:44 +0300

    Strip Comments

---
+++ Kata 4/Strip Comments/Solution.js
@@ -0,0 +1,10 @@
+const solution = (input, markers) => {
+  const result = input
+    .trimEnd()
+    .split("\n")
+    .map((s) => markers.reduce((t, m) => t.split(m)[0].trimEnd(), s));
+  if (result) {
+    return result.join("\n");
+  }
+  return result.join();
+};
