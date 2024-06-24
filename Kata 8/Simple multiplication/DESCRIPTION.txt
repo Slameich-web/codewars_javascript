cb1e7ed900e365dc951800fd3fac3c9a1af3765a
Slameich-web <slameacer@mail.ru>
2024-06-24 12:53:37 +0300

    Simple multiplication

---
+++ Kata 8/Simple multiplication/DESCRIPTION.txt
@@ -0,0 +1,2 @@
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
---
+++ Kata 8/Simple multiplication/Solution.js
@@ -0,0 +1,4 @@
+const simpleMultiplication = (number) => {
+  let result = number % 2 === 0;
+  return result === true ? number * 8 : number * 9;
+};
