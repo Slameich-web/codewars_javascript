7550838b5b817ef51c2ca2e9f47fa85bff7388c4
Slameich-web <slameacer@mail.ru>
2024-07-17 13:25:14 +0300

    Pick peaks

---
+++ Kata 5/Pick peaks/Solution.js
@@ -0,0 +1,14 @@
+const pickPeaks = (arr) => {
+  const result = { pos: [], peaks: [] };
+  for (let i = 1; i < arr.length - 1; i++) {
+    let next = 1;
+    while (arr[i] === arr[i + next]) {
+      next++;
+    }
+    if (arr[i] > arr[i - 1] && arr[i] > arr[i + next]) {
+      result.pos.push(i);
+      result.peaks.push(arr[i]);
+    }
+  }
+  return result;
+};
