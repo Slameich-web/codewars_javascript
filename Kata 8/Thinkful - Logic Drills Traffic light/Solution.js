106554f498e5ec8e386c502ad01003e2a9b02b20
Slameich-web <slameacer@mail.ru>
2024-09-24 15:01:36 +0300

    Thinkful - Logic Drills Traffic light

---
+++ Kata 8/Thinkful - Logic Drills Traffic light/Solution.js
@@ -0,0 +1,11 @@
+const updateLight = (current) => {
+  if (current === "green") {
+    return "yellow";
+  }
+  if (current === "yellow") {
+    return "red";
+  }
+  if (current === "red") {
+    return "green";
+  }
+};
