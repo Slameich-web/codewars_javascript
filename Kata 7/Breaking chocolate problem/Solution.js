460e3e26ee0dcc1c0282ccb3738f7918f49fd377
Slameich-web <slameacer@mail.ru>
2024-11-07 20:10:06 +0300

    kata 7

---
+++ Kata 7/Breaking chocolate problem/Solution.js
@@ -0,0 +1,7 @@
+const breakChocolate = (n, m) => {
+  const result = n * m - 1;
+  if (result > 0) {
+    return result;
+  }
+  return 0;
+};
---
+++ Kata 7/Sum of a sequence/Solution.js
@@ -0,0 +1,7 @@
+const sequenceSum = (begin, end, step) => {
+  let result = 0;
+  for (let i = begin; i <= end; i += step) {
+    result += i;
+  }
+  return result;
+};
