9ace034c3d47d4ee8bf1dc88649ac6c8f033cd85
Slameich-web <SlameACER@mail.ru>
2025-01-29 08:30:36 +0300

    kata 8

--- Kata 8/No zeros for heros/Solution.js
+++ Kata 8/No zeros for heros/Solution.js
@@ -1,7 +1,3 @@
 const noBoringZeros = (n) => {
-  if (n % 10 !== 0 || Math.abs(n) < 10) {
-    return n;
-  }
-
-  return noBoringZeros(Math.floor(n / 10));
+  return Number(String(n).replace(/0+$/, ""));
 };
